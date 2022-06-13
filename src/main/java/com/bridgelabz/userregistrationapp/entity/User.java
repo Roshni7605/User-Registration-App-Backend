a60db9a5eba052fecf7466c61b5c60fb81a27bf7
Roshni <roshni.mali@bridgelabz.com>
2022-06-13 07:59:46 +0530

    Added: User entity and to make password encoded using BCryptPasswordEncoder

--- src/main/java/com/bridgelabz/userregistrationapp/entity/User.java
+++ src/main/java/com/bridgelabz/userregistrationapp/entity/User.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.userregistrationapp.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class User {
+    
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String password;
+
 }
